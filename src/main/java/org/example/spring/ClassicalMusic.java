36d0c99d4308b614ffaae9577ed081b9e6463ff5
Oleg Fedotchev <herugeru@ya.ru>
2020-06-30 16:50:29 +0300

    Init and Destroy and Factory

--- src/main/java/org/example/spring/ClassicalMusic.java
+++ src/main/java/org/example/spring/ClassicalMusic.java
@@ -1,6 +1,19 @@
 package org.example.spring;
 
 public class ClassicalMusic implements Music {
+    private ClassicalMusic(){}
+
+    public static ClassicalMusic myFactory(){
+        return new ClassicalMusic();
+    }
+
+    public void myInit(){
+        System.out.println("doing my initialisation");
+    }
+    public void myDestroy(){
+        System.out.println("Doing my destroy");
+    }
+
     @Override
     public String getSong() {
         return "Hungarian Rapsody";
--- src/main/java/org/example/spring/TestSpring.java
+++ src/main/java/org/example/spring/TestSpring.java
@@ -23,5 +23,19 @@ public class TestSpring {
         System.out.println(musicPlayer.getName());
         System.out.println(musicPlayer.getVolume());
         musicPlayer.playAllMusic();
+
+        MusicPlayer secondMusicPlayer = context.getBean("musicPlayer",MusicPlayer.class);
+        System.out.println(musicPlayer == secondMusicPlayer);
+        System.out.println(musicPlayer);
+        System.out.println(secondMusicPlayer);
+        musicPlayer.setVolume(22);
+        System.out.println(musicPlayer.getVolume());
+        System.out.println(secondMusicPlayer.getVolume());
+
+        ClassicalMusic classicalMusic = context.getBean("classicalMusicBean",ClassicalMusic.class);
+        ClassicalMusic classicalMusic1 = context.getBean("classicalMusicBean",ClassicalMusic.class);
+        System.out.println(classicalMusic.getSong());
+
+        context.close();
     }
 }
